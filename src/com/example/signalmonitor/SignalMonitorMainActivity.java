1e44e5fb7225040556d83f834bb72b824d570238
mjohn706 <mjohn706@ford.com>
2013-10-17 19:23:54 -0700

    Further progress on code for milestones 1 and 2, complete code for 3 written, needs testing, but likely to work w/ only minor debug.

--- src/com/example/signalmonitor/SignalMonitorMainActivity.java
+++ src/com/example/signalmonitor/SignalMonitorMainActivity.java
@@ -4,16 +4,38 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONArray;
 
+import com.openxc.NoValueException;
 import com.openxc.VehicleManager;
+import com.openxc.measurements.AcceleratorPedalPosition;
+import com.openxc.measurements.BrakePedalStatus;
+import com.openxc.measurements.FuelConsumed;
+import com.openxc.measurements.FuelLevel;
+import com.openxc.measurements.HeadlampStatus;
+import com.openxc.measurements.HighBeamStatus;
+import com.openxc.measurements.IgnitionStatus;
+import com.openxc.measurements.Latitude;
+import com.openxc.measurements.Longitude;
 import com.openxc.measurements.Measurement;
+import com.openxc.measurements.Odometer;
+import com.openxc.measurements.ParkingBrakeStatus;
+import com.openxc.measurements.SteeringWheelAngle;
+import com.openxc.measurements.TorqueAtTransmission;
+import com.openxc.measurements.TransmissionGearPosition;
+import com.openxc.measurements.TurnSignalStatus;
 import com.openxc.measurements.UnrecognizedMeasurementTypeException;
+import com.openxc.measurements.VehicleButtonEvent;
+import com.openxc.measurements.VehicleDoorStatus;
 import com.openxc.measurements.VehicleSpeed;
+import com.openxc.measurements.EngineSpeed;
+import com.openxc.measurements.WindshieldWiperStatus;
 import com.openxc.remote.RawMeasurement;
 import com.openxc.remote.VehicleServiceException;
 
@@ -33,14 +55,14 @@ import android.widget.TextView;
 /**
  * 
  * @author mjohn706
- * For now, ignore the signal_name just read from JSON and always set up Listener for VehicleSpeed
+ *
  */
 public class SignalMonitorMainActivity extends Activity {
 
 	// per the tutorial, at object creation time:
-	private VehicleManager mVehicleManager;
+	private static VehicleManager mVehicleManager;
 	private boolean mBound = false;
-	private String TAG = "SignalMonitor";
+	private static String TAG = "SignalMonitor";
 	private String signal_threshold = "<"; // the default.
 
 	// It is a small file, it should not take long, so we read it in here:
@@ -54,7 +76,17 @@ public class SignalMonitorMainActivity extends Activity {
 	JSONObject watchersObject;
 	String signal_name;
 	String threshold_type;
+	String threshold_value;
+	String team_id;
     BufferedReader br = null;
+    
+    static String snapshot = null;
+    
+    Trigger[]  triggers = new Trigger[20]; // surely never more than this. // Obsolete already?
+    HashMap<String, Trigger> NamesToTriggers = new HashMap<String, Trigger>();
+    
+
+    int lineNo = 0;
 	{
 		try {
 		    br = new BufferedReader(new FileReader(watchersFile));
@@ -72,9 +104,10 @@ public class SignalMonitorMainActivity extends Activity {
 					watchersObject = new JSONObject(line);
 					Log.i(TAG, "created JSONObject " + watchersObject);
 					// read the easy ones: threshold_type, team_id
-					threshold_type = watchersObject.get("threshold_type").toString();
+ 					threshold_type = watchersObject.get("threshold_type").toString();
 					Log.i(TAG, 	"threshold_type: "	+ threshold_type);
-					Log.i(TAG, "team_id: " + watchersObject.get("team_id"));
+					team_id =  watchersObject.get("team_id").toString();
+					Log.i(TAG, "team_id: " + team_id);
 					
 				} catch (JSONException e) {
 					Log.e(TAG, "Exception reading JSONobject itself from one line of Watchers.txt: " + e.getMessage());
@@ -85,31 +118,50 @@ public class SignalMonitorMainActivity extends Activity {
 						@SuppressWarnings("unchecked")
 						Iterator<String> ourNamesIt = watchersObject.keys();
 						String candidateKey;
-						while (ourNamesIt.hasNext()) {
-							candidateKey = ourNamesIt.next();
-							if (candidateKey.equals("team_id")	|| candidateKey.equals("threshold_type")) {
-								ourNamesIt.remove();
-							} // at this point, watchersObject has only one name left, the signal name.
-							String won = watchersObject.names().toString();
-							Log.i(TAG, "watchersObject signal name: " + won); // alas, Android's JSON incomplete, so:
-							String wsb = new String(won.substring(2, won.length()-2));
-							Log.i(TAG, "watchersObject signal name as string: " + wsb); // alas, Android's JSON incomplete, so:
-							signal_name = wsb;
-
+					while (ourNamesIt.hasNext()) {
+						candidateKey = ourNamesIt.next();
+						if (candidateKey.equals("team_id")	|| candidateKey.equals("threshold_type")) {
+							ourNamesIt.remove();
 						}
+						}
+						// at this point, watchersObject has only one name
+							// left, the signal name.
+						String won = watchersObject.names().toString();
+						Log.i(TAG, "watchersObject signal name: " + won); // alas, Android's JSON incomplete, so:
+						String wsb = new String(won.substring(2, won.length() - 2));
+						Log.i(TAG, "watchersObject signal name as string: "	+ wsb); // alas, Android's JSON incomplete, so:
+						signal_name = wsb;
+						// at this point we have read in the whole line and set
+						// only wsb=signal_name and threshold_type. But we can
+						// use the signal_name to get the value, so:
+						// and now the all important value:
+						threshold_value = watchersObject.getString(signal_name);
+						
 					} catch (IllegalStateException e1) {
 						Log.e(TAG, "IllegalStateException reading from newly created JSONObject" + e1.getMessage());
 					} catch (UnsupportedOperationException boohoo) {
 						Log.e(TAG, "UnsupportedOperationException reading from newly created JSONObject" + boohoo.getMessage()); // we should never get here
+					} catch (JSONException e) {
+						Log.e(TAG, e.getMessage());
 					} finally {
 						Log.e(TAG, "Parsed JSON object for team_id, threshold_type and signal name");
 						//setListeners (threshold_type, signal_name); 
 					}
+					Log.i(TAG, "lineNo = " + lineNo);
+					triggers[lineNo] = new Trigger(signal_name, threshold_value, threshold_type);
+			        NamesToTriggers.put(signal_name, triggers[lineNo]);		
+					lineNo++;
 			}
+
+
 				} catch (IOException e) { Log.e(TAG, "Exception reading Watchers file" + e.getMessage());
 				} finally {
 					try {br.close();} catch(IOException ee) {Log.e(TAG, "Exception closing buffered reader"); } //TODO: obviate ts
 		Log.i(TAG, "Got it all");
+		Log.i(TAG, "Should be two triggers now:");
+		Log.i(TAG, "triggers[0] = " + triggers[0]);
+		Log.i(TAG, "triggers[1] = " + triggers[1]);
+		
 		// triggerFound();
 		/*Intent newIntent = new Intent(this, PostalService.class);
 		startService(newIntent);
@@ -147,7 +199,7 @@ public class SignalMonitorMainActivity extends Activity {
 			mVehicleManager = ((VehicleManager.VehicleBinder) service).getService();
 			// He forgot to say this 'try' would be necessary
 			try {
-				mVehicleManager.addListener(VehicleSpeed.class, mSpeedListener); // TODO:modify to read choice of signal from Watcher.txt and do condition test
+				mVehicleManager.addListener(VehicleSpeed.class, mSpeedListener);
 			} catch (VehicleServiceException e) {
 				Log.e(TAG, "Vehicle Service Exception " + e.toString());
 			} catch (UnrecognizedMeasurementTypeException e) {
@@ -168,20 +220,28 @@ public class SignalMonitorMainActivity extends Activity {
 	VehicleSpeed.Listener mSpeedListener = new VehicleSpeed.Listener() {
 		public void receive(Measurement measurement) {
 			final VehicleSpeed speed = (VehicleSpeed) measurement;
-			SignalMonitorMainActivity.this.runOnUiThread(new Runnable() {
-				public void run() {
-					mVehicleSpeedView.setText("Vehicle speed (km/h): "
-							+ speed.getValue().doubleValue());
-				}
-			});
+
 			// "do stuff with the measurement"
-			// what I do is test against a criterion.
-			if (signal_threshold.equals(">")) { // decide which test to do -- for now, just this one.
-				Log.i(TAG, "Testing for speed > " + Double.parseDouble(signal_threshold));
-				if (speed.getValue().doubleValue() < Double.parseDouble(signal_threshold)) {
-					triggerFound();
-				}
-			}
+			// what I do is test against a criterion, using my new Trigger class
+		    Trigger ourTrigger = NamesToTriggers.get("vehicle_speed");
+			Log.i(TAG, "Testing for speed " + ourTrigger.testCriterion + " speed");
+		    if (ourTrigger.test(speed.toString())) {
+                Log.i(TAG, "vehicle speed test passed");
+		    }
+		}
+	};
+
+	EngineSpeed.Listener mEngineSpeed = new EngineSpeed.Listener() {
+		public void receive(Measurement measurement) {
+			final EngineSpeed speed = (EngineSpeed) measurement;
+
+			// "do stuff with the measurement"
+			// what I do is test against a criterion, using my new Trigger class
+		    Trigger ourTrigger = NamesToTriggers.get("vehicle_speed");
+			Log.i(TAG, "Testing for engine speed " + ourTrigger.testCriterion + " speed");
+		    if (ourTrigger.test(speed.toString())) {
+                Log.i(TAG, "engine speed test passed");
+		    }
 		}
 	};
 
@@ -251,4 +311,70 @@ try {
 		
 	}
 
+	public void registerListener(String signalName, String threshold,
+			String thresholdValue) {
+		try {
+			if (signalName.equals("vehicle_speed"))
+				mVehicleManager.addListener(VehicleSpeed.class, mSpeedListener);
+			else if (signalName.equals("engine_speed"))
+				mVehicleManager.addListener(EngineSpeed.class, mEngineSpeed);
+			else
+				Log.e(TAG, "We should never get here");
+		} catch (Exception e) {
+			Log.e(TAG, "Exception in registerListener: " + e.getMessage());
+		}
+	}
+
+	static public void makeSnapshot() {
+		StringBuffer sb = new StringBuffer();
+		try {
+			VehicleSpeed rawSpeed = (VehicleSpeed) mVehicleManager.get(VehicleSpeed.class);
+			sb.append(rawSpeed.serialize()); // repeat fr ea msrt
+			TorqueAtTransmission rawTorque = (TorqueAtTransmission) mVehicleManager.get(TorqueAtTransmission.class);
+			AcceleratorPedalPosition rawPedal = (AcceleratorPedalPosition) mVehicleManager.get(AcceleratorPedalPosition.class);
+			sb.append(rawPedal.serialize());
+			BrakePedalStatus rawBrake = (BrakePedalStatus) mVehicleManager.get(BrakePedalStatus.class);
+			sb.append(rawBrake.serialize());
+			EngineSpeed rawRPM = (EngineSpeed) mVehicleManager.get(EngineSpeed.class);
+			sb.append(rawRPM.serialize());
+			FuelConsumed used = (FuelConsumed) mVehicleManager.get(FuelConsumed.class);
+			sb.append(used.serialize());
+			FuelLevel left = (FuelLevel) mVehicleManager.get(FuelLevel.class);
+			sb.append(left.serialize());
+			HeadlampStatus lights = (HeadlampStatus) mVehicleManager.get(HeadlampStatus.class);
+			sb.append(lights.serialize());
+			HighBeamStatus brights = (HighBeamStatus) mVehicleManager.get(HighBeamStatus.class);
+			sb.append(brights.serialize());
+			IgnitionStatus key = (IgnitionStatus) mVehicleManager.get(IgnitionStatus.class);
+			sb.append(key.serialize());
+			Latitude lat = (Latitude) mVehicleManager.get(Latitude.class);
+			sb.append(lat.serialize());
+			Longitude lon = (Longitude) mVehicleManager.get(Longitude.class);
+			sb.append(lon.serialize());
+			Odometer odo = (Odometer) mVehicleManager.get(Odometer.class);
+			sb.append(odo.serialize());
+			ParkingBrakeStatus pbs = (ParkingBrakeStatus) mVehicleManager.get(ParkingBrakeStatus.class);
+			sb.append(pbs.serialize());
+			SteeringWheelAngle swa = (SteeringWheelAngle) mVehicleManager.get(SteeringWheelAngle.class);
+			sb.append(swa.serialize());
+			TransmissionGearPosition tgp = (TransmissionGearPosition) mVehicleManager.get(TransmissionGearPosition.class);
+			sb.append(tgp.serialize());
+			TurnSignalStatus tss = (TurnSignalStatus) mVehicleManager.get(TurnSignalStatus.class);
+			sb.append(tss.serialize());
+			VehicleButtonEvent vbe = (VehicleButtonEvent) mVehicleManager.get(VehicleButtonEvent.class);
+			sb.append(vbe.serialize());
+			VehicleDoorStatus vds = (VehicleDoorStatus) mVehicleManager.get(VehicleDoorStatus.class);
+			sb.append(vds.serialize());
+			WindshieldWiperStatus wws = (WindshieldWiperStatus) mVehicleManager.get(WindshieldWiperStatus.class);
+			sb.append(wws.serialize());
+            snapshot = sb.toString();
+		} catch (NoValueException e) {
+			Log.w(TAG, "The vehicle may not have made the measurement yet");
+		} catch (UnrecognizedMeasurementTypeException e) {
+			Log.w(TAG, "The measurement type was not recognized");
+		}
+	}
+
+	static public void uploadSnapshot() {
+	} // void for now
 }
---
+++ src/com/example/signalmonitor/Trigger.java
@@ -0,0 +1,51 @@
+package com.example.signalmonitor;
+
+import android.util.Log;
+
+/**                                                                                                                                               
+* Trigger exists mainly to support test() which                                                                                                  
+* allows testing for whether or not threshold (trigger) is met                                                                                   
+* for all types: '<', '>', true/false, state                                                                                                     
+* TODO: make sure I know where to set all these. Provide accessors                                                                               
+* in case I need thread safe?                                                                                                                    
+ */
+public class Trigger {
+	private String name;
+	private String value;
+	public String testCriterion; // what before I called 'threshold_type'
+	private String TAG = "SignalMonitor";
+	
+
+	Trigger(String name, String value, String tCrit) {
+		this.name = name;
+		this.value = value;
+		this.testCriterion = tCrit;
+	}
+
+	/**
+	 * I think I can make this private, since it will only be in
+	 * uploadConditionally()
+	 * 
+	 * Status: works
+	 */
+	public boolean test(String val) {
+		if (this.testCriterion == "<") {
+			return (Double.parseDouble(this.value) < Double.parseDouble(val));
+		} else if (this.testCriterion == ">") {
+			return (Double.parseDouble(this.value) < Double.parseDouble(val));
+		} else if (this.testCriterion == "boolean") {
+			return (this.value.equals(true));
+		} else if (this.testCriterion == "state") { // e.g. test(gear_pos, "first")
+			return (this.value.equals(val));
+		} else
+			return false;
+	}
+	public void uploadConditionally(Trigger aTrigger, String value) {
+	    if (aTrigger.test(value)) {
+	        Log.i(TAG, "Speed threshold met");
+	         com.example.signalmonitor.SignalMonitorMainActivity.makeSnapshot();
+	         com.example.signalmonitor.SignalMonitorMainActivity.uploadSnapshot();
+	    }
+	}
+
+}
