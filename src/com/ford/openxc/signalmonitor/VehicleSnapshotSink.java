6c1d16631323b881f086513133e627877b55a592
TJ Giuli <tgiuli@ford.com>
2013-10-19 01:05:15 -0700

    Upload a JSON array rather than a concatenated list of JSON objects

--- src/com/ford/openxc/signalmonitor/VehicleSnapshotSink.java
+++ src/com/ford/openxc/signalmonitor/VehicleSnapshotSink.java
@@ -1,12 +1,22 @@
 package com.ford.openxc.signalmonitor;
 
+import java.io.IOException;
+import java.io.StringWriter;
 import java.util.HashMap;
+import java.util.Iterator;
 
+import android.util.Log;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.openxc.remote.RawMeasurement;
 import com.openxc.sinks.BaseVehicleDataSink;
 import com.openxc.sinks.DataSinkException;
 
 public class VehicleSnapshotSink extends BaseVehicleDataSink {
+    
+    public static final String TAG = "VehicleSnapshotSink";
+    
     private HashMap<String, RawMeasurement> mMeasurements =
             new HashMap<String, RawMeasurement>();
 
@@ -15,29 +25,41 @@ public class VehicleSnapshotSink extends BaseVehicleDataSink {
         mMeasurements.put(measurement.getName(), measurement);
         return true;
     }
+    /**
+     * Given an iterator over a collection of serialized JSON objects, 
+     * returns a serialized JSON array.
+     * @param recordIterator An iterator over a collection of strings representing JSON objects
+     * @return A serialized JSON array
+     * @throws IOException
+     */
+    private String objectsToArray(Iterator<RawMeasurement> recordIterator)
+        throws IOException {
+        StringWriter buffer = new StringWriter(512);
+        JsonFactory jsonFactory = new JsonFactory();
 
-    // TODO This is returning a mangy string that's not a JSON in and of itself
-    // - a better way would be to generate a single JSON object, e.g.:
-    //
-    // {'vehicle_speed': 42,
-    //   'transmission_gear_position: 'first',
-    //   ...
-    // }
-    //
-    // or a JSON object wtih an array of all of the serialized measurements:
-    //
-    // [{"name": "steering_wheel_angle", "value": 42},
-    //     {"name": "parking_brake_status", "value": false}]
-    //
-    // This array is what the UploaderSink creates, so you could just copy the
-    // code from there but you would have to make sure the server side was
-    // looking for this format first.
-    //
-    public synchronized String generateSnapshot() {
-        StringBuffer sb = new StringBuffer();
-        for(RawMeasurement measurement : mMeasurements.values()) {
-            sb.append(measurement.serialize());
+        JsonGenerator gen = jsonFactory.createJsonGenerator(buffer);
+
+        gen.writeStartObject();
+        gen.writeArrayFieldStart("records");
+        while(recordIterator.hasNext()) {
+            gen.writeRaw(recordIterator.next().serialize());
+            if(recordIterator.hasNext()) {
+                gen.writeRaw(",");
+            }
         }
-        return sb.toString();
+        gen.writeEndArray();
+        gen.writeEndObject();
+
+        gen.close();
+        return buffer.toString();
+    }
+    
+    public synchronized String generateSnapshot() {
+        try {
+            return objectsToArray(mMeasurements.values().iterator());
+        } catch (IOException e) {
+            Log.e(TAG, e.getMessage());
+            return null;
+        }
     }
 }
